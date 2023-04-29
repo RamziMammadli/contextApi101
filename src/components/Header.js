4e9862fef7809fd6fc284050474694e1f334889f
RamziMammadli <remzimemmedli95@gmail.com>
2023-04-29 22:41:36 +0400

    setColorBG

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,22 +1,30 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import { MainContext } from "../context/context";
 
 const Header = () => {
   const {name, surname, ab, setAb} = useContext(MainContext);
+  const [ bgcolor , setColor ] = useState("orange")
 
   const Change = () => {
-    
-    setAb(ab === "Sara" ? "Sitare" : "Sara")
+
+    setAb(ab === "Sara" ? "Sitare" : "Sara") // ternary operator
+  }
+
+  const SwitchBG = () => {
+    setColor(bgcolor === "orange" ? "yellow" : "orange")
   }
 
   return (
-    <div style={{ height: 300, backgroundColor: "orange" }}>
+    <div style={{ height: 300, backgroundColor: bgcolor }}>
       <p>{name}</p>
       <p>{surname}</p>
       <hr/>
       <h1>{ab}</h1>
       <button onClick={Change}>
         Change
+      </button>
+      <button onClick={SwitchBG}>
+        Switch Background
       </button>
     </div>
   );
